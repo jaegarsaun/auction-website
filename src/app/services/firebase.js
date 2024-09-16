6859714a2593417f58d71091c391b3e94281db6f
Jaegar <jaegarls@icloud.com>
2024-09-16 21:18:07 -0230

    using env

--- src/app/services/firebase.js
+++ src/app/services/firebase.js
@@ -3,20 +3,16 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Use environment variables for Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyC39-o7kpffP3Frt2aVtyhKGagw09zqtaU",
-  authDomain: "auction-bac9b.firebaseapp.com",
-  projectId: "auction-bac9b",
-  storageBucket: "auction-bac9b.appspot.com",
-  messagingSenderId: "421575764356",
-  appId: "1:421575764356:web:0259d3198fb24e55bf55d1",
-  measurementId: "G-WV2NDB4ZLH",
-};
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
